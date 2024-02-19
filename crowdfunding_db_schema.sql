15ddaadffeb60101586a2208eca57b95ffb29d3c
Tyson <tyson@horsewell.me>
2024-02-19 20:59:27 +0800

    Database

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE crowdfunding_db;
+
 -- drop tables if exist
 
 drop table if exists contacts;
@@ -9,25 +11,25 @@ drop table if exists campaign;
 
 CREATE TABLE contacts (
     contact_id int NOT NULL,
-    first_name varchar (150)  NOT NULL,
-    last_name varchar (150)  NOT NULL,
-    email varchar (150)  NOT NULL,
+    first_name varchar (30)  NOT NULL,
+    last_name varchar (30)  NOT NULL,
+    email varchar (60)  NOT NULL,
 	primary key (contact_id)
 );
 
 -- create category table and set our primary key
 
 CREATE TABLE category (
-    category_id varchar (150)   NOT NULL,
-    category varchar (150)  NOT NULL,
+    category_id varchar (6)   NOT NULL,
+    category varchar (18)  NOT NULL,
 	primary key (category_id)
 );
 
 -- create subcategory table and set our primary key
 
 CREATE TABLE subcategory (
-    subcategory_id varchar (150)  NOT NULL,
-    subcategory varchar (150)  NOT NULL,
+    subcategory_id varchar (10)  NOT NULL,
+    subcategory varchar (20)  NOT NULL,
 	primary key (subcategory_id)
 );
 
@@ -36,25 +38,32 @@ CREATE TABLE subcategory (
 CREATE TABLE campaign (
     cf_id int NOT NULL,
     contact_id int NOT NULL,
-    company_name varchar (300)  NOT NULL,
-    description varchar (300)  NOT NULL,
+    company_name varchar (60)  NOT NULL,
+    description varchar (60)  NOT NULL,
     goal float   NOT NULL,
     pledged float   NOT NULL,
-    outcome varchar (300)  NOT NULL,
+    outcome varchar (12)  NOT NULL,
     backers_count int   NOT NULL,
-    country varchar (300)  NOT NULL,
-    currency varchar (300)  NOT NULL,
+    country varchar (6)  NOT NULL,
+    currency varchar (6)  NOT NULL,
     launched_date date   NOT NULL,
     end_date date   NOT NULL,
-    category_id varchar (300)  NOT NULL,
-    subcategory_id varchar (300)  NOT NULL,
+    category_id varchar (6)  NOT NULL,
+    subcategory_id varchar (10)  NOT NULL,
 	primary key (cf_id),
 	foreign key (contact_id) references contacts(contact_id),
 	foreign key (category_id) references category(category_id),
 	foreign key (subcategory_id) references subcategory(subcategory_id)
 );
 
-select * from contacts
-select * from category
-select * from subcategory
+-- test 
+select * from contacts;
+select * from category;
+select * from subcategory;
+select * from campaign;
+
+-- test joins
 select * from campaign
+left join contacts on campaign.contact_id = contacts.contact_id
+left join category on campaign.category_id = category.category_id
+left join subcategory on campaign.subcategory_id = subcategory.subcategory_id;
